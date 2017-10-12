99cbba954993e66d64b48fc5df9c5c305329a16f
SiM <santiaggoo@gmail.com>
2017-10-12 03:30:56 -0300

    tensorboard added to visualize the graph and summarize loss and accuracy

--- sentiment_rnn.py
+++ sentiment_rnn.py
@@ -11,13 +11,15 @@ vec_size = 300
 batch_size = 128
 number_of_layers= 2
 number_of_classes= 3
+steps = 100000
+
+TRAIN_DIR="/checkpoints"
 
 # this just makes sure that all our following operations will be placed in the right graph.
 tf.reset_default_graph()
 
 # create a session variable that we can run later.
 session = tf.Session()
-
 # make placeholders for data we'll feed in
 tweets = tf.placeholder(tf.float32, [None, tweet_size, vec_size], "tweets")
 labels = tf.placeholder(tf.float32, [None, number_of_classes], "labels")
@@ -31,23 +33,31 @@ multi_lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(numbe
 #Creates a recurrent neural network
 _, final_state = tf.nn.dynamic_rnn(multi_lstm_cells, tweets, dtype=tf.float32)
 
-#weight and bias to shape the final layer
-W = tf.get_variable("weight_matrix", [hidden_size, number_of_classes], tf.float32, tf.random_normal_initializer(stddev=1.0 / math.sqrt(hidden_size)))
-b = tf.get_variable("bias", [number_of_classes], initializer=tf.constant_initializer(1.0))
+with tf.name_scope("final_layer"):
+  #weight and bias to shape the final layer
+  W = tf.get_variable("weight_matrix", [hidden_size, number_of_classes], tf.float32, tf.random_normal_initializer(stddev=1.0 / math.sqrt(hidden_size)))
+  b = tf.get_variable("bias", [number_of_classes], initializer=tf.constant_initializer(1.0))
 
 sentiments = tf.matmul(final_state[-1][-1], W) + b
 
+with tf.name_scope("loss"):
+  # define cross entropy loss function
+  losses = tf.nn.softmax_cross_entropy_with_logits(logits=sentiments, labels=labels)
+  loss = tf.reduce_mean(losses)
 
-# define cross entropy loss function
-losses = tf.nn.softmax_cross_entropy_with_logits(logits=sentiments, labels=labels)
-loss = tf.reduce_mean(losses)
-
-# round our actual probabilities to compute error
-accuracy = tf.to_float(tf.equal(tf.argmax(prob,1), tf.argmax(labels,1)))
-accuracy = tf.reduce_mean(tf.cast(accuracy, dtype=tf.float32))
+with tf.name_scope("accuracy"):
+  # round our actual probabilities to compute error
+  accuracy = tf.to_float(tf.equal(tf.argmax(prob,1), tf.argmax(labels,1)))
+  accuracy = tf.reduce_mean(tf.cast(accuracy, dtype=tf.float32))
 
 # define our optimizer to minimize the loss
-optimizer = tf.train.AdamOptimizer().minimize(loss)
+with tf.name_scope("train"):
+  optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+#tensorboard summaries
+merged_summary = tf.summary.merge_all()
+logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, session.graph)
 
 # initialize any variables
 tf.global_variables_initializer().run(session=session)
@@ -77,6 +87,10 @@ for step in range(steps):
     test_accuracy = []
     print("Train loss:", loss_train)
     print("Train accuracy:%.3f%%" % (accuracy_train*100))
+
+    summary = session.run(merged_summary, feed_dict=data)
+    print("Summary steps:", step)
+    writer.add_summary(summary, step)
 
     for batch_num in range(int(len(test_tweets)/batch_size)):
       test_offset = (batch_num * batch_size) % (len(test_tweets) - batch_size)
