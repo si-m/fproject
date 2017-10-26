235c22e243f6975e7bcdc33294f3e841029aab3c
SiM <santiaggoo@gmail.com>
2017-10-26 04:25:24 -0300

    Now build_dataset.py checks for same distribution of sentiments in the dataset.

--- data_process/build_dataset.py
+++ data_process/build_dataset.py
@@ -3,15 +3,17 @@ import sys, getopt
 import numpy as np
 import json
 from json_tricks.np import dump, dumps, load, loads, strip_comments
-
+import random
 
 def main(argv):
 	inputfile = ''
 	trainfile = 'pre_train_set.json'
 	testfile  = 'pre_test_set.json'
-	filtered_set  = []
-	train_set = []
-	test_set = []
+	positive_set, negative_set, neutral_set, export_set = [], [], [], []
+	set_size = 0
+	train_set, test_set = [], []
+	uniq = set()
+
 	try:
 		opts, args = getopt.getopt(argv,"hi:",["ifile="])
 	except getopt.GetoptError:
@@ -24,8 +26,6 @@ def main(argv):
 		elif opt in ("-i", "--ifile"):
 			inputfile = arg
 
-	uniq = set()
-
 	with open(inputfile, 'r') as fpt:
 		l = fpt.readlines()
 	for row, d in enumerate(l):
@@ -34,12 +34,40 @@ def main(argv):
 		except TypeError:
 			raw = json.loads(d)
 		if(raw['klass'] != 'NONE' and (raw['id'] not in uniq)):
-			filtered_set.append({'text': raw['text'], 'klass': raw['klass']})
 			uniq.add(raw['id'])
-	  	
-	half = len(filtered_set) >> 1
-	train_set = filtered_set[:half]
-	test_set  = filtered_set[half:]
+			if raw['klass'] == "positive":
+				positive_set.append({'text': raw['text'], 'klass': raw['klass']})
+			if raw['klass'] == "negative":
+				negative_set.append({'text': raw['text'], 'klass': raw['klass']})
+			if raw['klass'] == "neutral":
+				neutral_set.append({'text': raw['text'], 'klass': raw['klass']})
+
+	# check sizes
+	print("Uniques tweets: ",len(uniq))
+	print("Positive tweets:", len(positive_set))
+	print("Negative tweets:", len(negative_set))
+	print("Neutral tweets:", len(neutral_set))
+
+
+	# smallest
+	set_size = min(len(positive_set), len(negative_set), len(neutral_set))
+
+	#resize
+	positive_set = positive_set[:set_size]
+	neutral_set = neutral_set[:set_size]
+	negative_set = negative_set[:set_size]
+
+	#concat
+	export_set = positive_set + negative_set + neutral_set
+
+	#shuffle the dataset
+	random.shuffle(export_set)
+	random.shuffle(export_set)
+
+	#split in training and testing sets
+	half = len(export_set) >> 1	
+	train_set = export_set[:half]
+	test_set  = export_set[half:]
 
 	with open(trainfile, 'w') as outfile:
 			json_data = dumps(train_set)
