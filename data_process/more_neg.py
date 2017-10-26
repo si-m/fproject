929fa0906255f7adf8006e2259988526bcfb52b1
SiM <santiaggoo@gmail.com>
2017-10-26 05:46:52 -0300

    New program to gen more negative tweets from txt dataset.

---
+++ data_process/more_neg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys, getopt
+import numpy as np
+import json
+from json_tricks.np import dump, dumps, load, loads, strip_comments
+import random
+
+def main(argv):
+	negative_file = 'tweets_neg_clean.txt'
+	neg_outfile = 'neg_set.json'
+	set_size = 0
+	neg_set = []
+	uniq = set()
+
+	with open(negative_file, 'r') as fpt:
+		l = fpt.readlines()
+	for row, d in enumerate(l):
+		neg_set.append({"text": d, "klass": "negative", "id": row})
+
+	with open(neg_outfile, 'w') as outfile:
+		for tweet in neg_set:
+			json.dump(tweet, outfile)
+			outfile.write('\n')
+		outfile.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
