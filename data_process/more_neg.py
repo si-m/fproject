d7a06d2f8c8de22ce824e985ab5d5b07ce249c54
SiM <santiaggoo@gmail.com>
2017-10-26 20:24:32 -0300

    resize spanish_dataset_more_neg

--- data_process/more_neg.py
+++ data_process/more_neg.py
@@ -6,16 +6,15 @@ from json_tricks.np import dump, dumps, load, loads, strip_comments
 import random
 
 def main(argv):
-	negative_file = 'tweets_neg_clean.txt'
+	negative_file = 'spanish_tweets_neg_clean.txt'
 	neg_outfile = 'neg_set.json'
 	set_size = 0
 	neg_set = []
-	uniq = set()
 
 	with open(negative_file, 'r') as fpt:
 		l = fpt.readlines()
 	for row, d in enumerate(l):
-		neg_set.append({"text": d, "klass": "negative", "id": row})
+		neg_set.append({"text": d.rstrip(), "klass": "negative", "id": row})
 
 	with open(neg_outfile, 'w') as outfile:
 		for tweet in neg_set:
